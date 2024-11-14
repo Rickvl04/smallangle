b53fd3c9b373bc29df1da5198e9db9888fbadb25
Rickvl04 <rickvl@kpnmail.nl>
2024-11-14 10:33:35 +0100

    commit command groups
    Added a command group with sin() and tan() as subcommands

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,13 +3,19 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
+@click.group()
+def cmd_group():
+    pass
 
+@cmd_group.command()
+@click.argument("number")
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-
+@cmd_group.command()
+@click.argument("number")
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
